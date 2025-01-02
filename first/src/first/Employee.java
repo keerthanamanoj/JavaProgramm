faea7273ac3d7f282276edb69e2609a022476e37
Admin <keerthanamanoj>
2025-01-02 23:37:20 +0530

    allFiles

--- first/src/first/Employee.java
+++ first/src/first/Employee.java
@@ -3,11 +3,13 @@ package first;
 public class Employee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		// Finding sum of first 10 natural numbers
+		int sum = 0;
+		for (int i = 1; i <= 10; i++) {
+			sum = sum + i;
+		}
+		System.out.println("Sum is " + sum);
+
 	}
 
 }
-
-		
-		
